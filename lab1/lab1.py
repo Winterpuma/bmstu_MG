acd43f7f6aadb9d287eb59e324e5ff207a99723c
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2020-02-13 14:26:52 +0300

    Fix output

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,15 +1,31 @@
 from math import ceil, sqrt
 
+def output(s):
+    if type(s) == float:
+        if s > 1000000:
+            return '{:.8e}'.format(s)
+        return '{:.8f}'.format(s)
+    elif type(s) == int:
+        return str(s)
+    else:
+        return s
+
 def func(x, u):
     return x ** 2  + u ** 2
 
 def euler(n, h, x, y):
     y_out = []
     for i in range(n):
-        y += h * func(x, y)
-        x += h
-        #print(x)
-        y_out.append(y)
+        try:
+            y += h * func(x, y)
+            y_out.append(y)
+            x += h
+        except OverflowError:
+            y_out.append('overflow')
+            for j in range(i, n-1):
+                y_out.append('-----')
+            break
+        
     return y_out
 
 def implicit_euler(n, h, x, y):
@@ -17,11 +33,12 @@ def implicit_euler(n, h, x, y):
     for i in range(n):
         D = 1 - 4*h*(y + h*((x + h)**2))
         if D < 0:
-            y_out.append('D<0')
-            continue
+            y_out.append('D < 0')
+            for j in range(i, n-2):
+                y_out.append('-----')
+            break
         y = (1 - sqrt(D)) / (2*h)
         x += h
-        #print(x)
         y_out.append(y)
     return y_out
 
@@ -40,7 +57,6 @@ def picar(n, h, x):
     y_out = [[0, 0]]
     for i in range(n-1):
         x += h
-        #print(x)
         y_f3 = f3(x)
         y_out.append([y_f3, f4(x, y_f3)])
     return y_out
@@ -50,18 +66,20 @@ def work():
     h = 10 ** -2
     x = 0
     y0 = 0
-    end = 2
+    end = 3
 
-    n = ceil(abs(end - x)/h) # количество повторений
+    n = ceil(abs(end - x)/h)+1 # количество повторений
 
     x_arr = [x + h*i for i in range(n)]
     y1 = euler(n, h, x, y0)
     y2 = implicit_euler(n, h, x, y0)
     y3 = picar(n, h, x)
 
-    print("|    x   |    Пикара 3   |     Пикара 4   |     Явный    |    Неявный     |")
-    for i in range(0, len(y1)):
-        print("|{:.5f} |  {:.8f}   |   {:.8f}   |  {:.8f}  |   {:.8f}   |".format(x_arr[i],y3[i][0],y3[i][1],y1[i],y2[i]))
+    print(len(y1), len(y2), len(y3))
+
+    print("|    x    |   Пикара 3    |    Пикара 4   |     Явный     |    Неявный    |")
+    for i in range(n):
+        print("|{:^9.5f}|{:^15.8f}|{:^15.8f}|{:^15s}|{:^15s}|".format(x_arr[i],y3[i][0],y3[i][1],output(y1[i]),output(y2[i])))
 
 
 work()
